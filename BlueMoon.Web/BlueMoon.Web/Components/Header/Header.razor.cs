b1c1843e85c570f5929d13372f24b09f1b28994a
douglas-i <hdouglasu@gmail.com>
2024-10-23 18:36:17 -0600

    Se migra el proyecto a BlazorWebAssembly

---
+++ BlueMoon.Web/BlueMoon.Web/Components/Header/Header.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlueMoon.Web.Components.Header
+{
+    public partial class Header
+    {
+        [Inject]
+        private NavigationManager NavigationManager { get; set; }
+        private void OpenPdfInNewTab()
+        {
+            var pdfUrl = "/catalogo";
+            // Abrir la URL en una nueva pestaña
+            NavigationManager.NavigateTo(pdfUrl, true);
+        }
+        private void Home()
+        {
+            var pdfUrl = "/";
+            // Abrir la URL en una nueva pestaña
+            NavigationManager.NavigateTo(pdfUrl, true);
+        }
+        private void Contact()
+        {
+            var pdfUrl = "/contacto";
+            // Abrir la URL en una nueva pestaña
+            NavigationManager.NavigateTo(pdfUrl, true);
+        }
+    }
+}
